4363c38f332ddd4959b66208fa838cce2c88ca2e
Samuel BS <70033730+Amongog@users.noreply.github.com>
2021-11-26 19:24:24 -0600

    Programa para generar permutaciones de un string.

--- laboratorio2.c
+++ laboratorio2.c
@@ -2,5 +2,21 @@
 //                IE-0117 Programación Bajo Plataformas Abiertas
 //            Autor: Samuel Berrocal Soto. samuel.berrocal@ucr.ac.cr
 // -----------------------------------------------------------------------------
-// Programa para
+// Programa para generar permutaciones de un string.
 // -----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+
+  int i;
+
+  printf("Cantidad de argumentos: %d\n", argc);
+
+  for ( i = 0; i < argc; i++) {
+    printf("Posición del argumento: %d, valor: %s\n", i, argv[i]);
+  }
+
+  return 0;
+}
